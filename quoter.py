d33b5ef0c26878b88c37dfd018e2bf2460e0bc40
Alexey Leshchenko <leshchenko@gmail.com>
2021-02-04 17:15:49 +0300

    Добавить `vehicle_subtype`

--- quoter.py
+++ quoter.py
@@ -1,9 +1,10 @@
+from typing import Optional
 from flask import Flask, make_response, request
 from icontract import ViolationError, require
 from numpy_financial import pmt
 from pydantic import BaseModel, ValidationError, validator
 
-UPDATE_DATE = "2021-02-03"
+UPDATE_DATE = "2021-02-04"
 
 app = Flask(__name__)
 
@@ -23,6 +24,7 @@ def get_quote():
 
 class CalcParams(BaseModel):
     vehicle_type: str
+    vehicle_subtype: Optional[str]
     brand: str
     year: int
     price: int
@@ -31,6 +33,10 @@ class CalcParams(BaseModel):
 
     @validator("vehicle_type")
     def vehicle_type_lower(cls, v):
+        return v.strip().lower()
+
+    @validator("vehicle_subtype")
+    def vehicle_subtype_lower(cls, v):
         return v.strip().lower()
 
     @validator("brand")
@@ -42,6 +48,7 @@ def check_params(params: CalcParams):
 
     CHECKS = [
         check_vehicle_type,
+        check_vehicle_subtype,
         check_numericals,
         check_year_for_russian_vehicles,
         no_chinese_brands,
@@ -59,10 +66,86 @@ def check_vehicle_type(vehicle_type, **args):
     pass
 
 
-@require(lambda year: 2016 <= year <= 2021)
-@require(lambda downpayment: 0 <= downpayment < 0.5)
-@require(lambda price: 1_000_000 <= price <= 20_000_000)
-def check_numericals(year, downpayment, price, **args):
+ATI_SUBTYPES = {
+    "1": "тентованный",
+    "2": "контейнер",
+    "3": "фургон",
+    "4": "цельнометалл.",
+    "5": "изотермический",
+    "6": "рефрижератор",
+    "7": "реф. мультирежимный",
+    "8": "реф. с перегородкой",
+    "9": "реф.-тушевоз",
+    "10": "бортовой",
+    "11": "открытый конт.",
+    "12": "площадка без бортов",
+    "13": "самосвал",
+    "14": "шаланда",
+    "15": "низкорамный",
+    "16": "низкорам.платф.",
+    "17": "телескопический",
+    "18": "трал",
+    "19": "балковоз(негабарит)",
+    "20": "автобус",
+    "21": "автовоз",
+    "22": "автовышка",
+    "23": "автотранспортер",
+    "24": "бетоновоз",
+    "25": "битумовоз",
+    "26": "бензовоз",
+    "27": "вездеход",
+    "28": "газовоз",
+    "29": "зерновоз",
+    "30": "коневоз",
+    "31": "конт.площадка",
+    "32": "кормовоз",
+    "33": "кран",
+    "34": "лесовоз",
+    "35": "ломовоз",
+    "36": "манипулятор",
+    "37": "микроавтобус",
+    "38": "муковоз",
+    "39": "панелевоз",
+    "40": "пикап",
+    "41": "пухтовоз",
+    "42": "пирамида",
+    "43": "рулоновоз",
+    "44": "седельный тягач",
+    "45": "скотовоз",
+    "46": "стекловоз",
+    "47": "трубовоз",
+    "48": "цементовоз",
+    "49": "цистерна",
+    "50": "щеповоз",
+    "51": "эвакуатор",
+    "52": "грузопассажирский",
+    "53": "клюшковоз",
+    "54": "мусоровоз",
+    "55": "юмбо",
+}
+
+ALLOWED_SYBTYPES = {
+    "1": "тентованный",
+    "5": "изотермический",
+    "6": "рефрижератор",
+    "7": "реф. мультирежимный",
+    "8": "реф. с перегородкой",
+    "9": "реф.-тушевоз",
+    "10": "бортовой",
+    "13": "самосвал",
+    "44": "седельный тягач",
+}
+
+
+@require(
+    lambda vehicle_subtype: vehicle_subtype in ATI_SUBTYPES.values() if vehicle_subtype else True,
+    "This vehicle_subtype is unknown",
+)
+@require(
+    lambda vehicle_subtype: vehicle_subtype in ALLOWED_SYBTYPES.values() if vehicle_subtype else True,
+    "This vehicle_subtype is forbidden",
+)
+def check_vehicle_subtype(vehicle_subtype, **args):
     pass
 
 
@@ -96,6 +179,13 @@ def check_year_for_russian_vehicles(vehicle_type, brand, year, **args):
     "Only Russian and European brands are allowed, no Chinese",
 )
 def no_chinese_brands(brand, **args):
+    pass
+
+
+@require(lambda year: 2016 <= year <= 2021)
+@require(lambda downpayment: 0 <= downpayment < 0.5)
+@require(lambda price: 1_000_000 <= price <= 20_000_000)
+def check_numericals(year, downpayment, price, **args):
     pass
 
 
